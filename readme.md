bbf643893f9621fa0057f3bbf9f4fff358292b19
Tom Townsend <tom.towny@gmail.com>
2013-02-16 21:46:04 +0000

    Barebones commit with working intial version

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Personal Shopper
+
+## Purpose
+This application is designed to run on a internet addressable server in concert with Twilio to provide an easy way to remember the shopping
+
+
+## Usage
+
+App is run by <code>node server.js</code> on the target machine
+
+Then just text the Twilio number you specified in the config to add items to the list. Text list to the number to have the list text back to you.
---
+++ server.js
@@ -0,0 +1,55 @@
+/*
+	Author : Tom Townsend @tomtowny
+
+*/
+var express = require('express');
+var http = require('http');
+var creds = require('./config').Credentials;
+var client = require('twilio')(creds.sid,creds.authToken);
+var Twiml = require('twilio').Twiml;
+var app = express();
+var shoppingList = [];
+var oldMessage = 'No current list available.';
+
+// configure express
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.use(app.router);
+});
+
+function sendList(){
+// serve the list to the client
+	var message = '';
+	// loop over the shopping list to push the items to the message
+	if(shoppingList.length == 0){
+		message = oldMessage;
+	}else{
+		while(shoppingList.length != 0){
+			message = message.concat(shoppingList.pop()+'\n');
+		}
+	}
+	client.sendSms({to:creds.incoming,from:creds.outgoing,body:message},function(err, responseData){
+		if(err){
+			console.log(err);
+		}else{
+			console.log("Shopping list sent.");
+			oldMessage = message;
+			message = '';
+		}
+	});
+}
+
+app.post('/list', function(req,res){
+	// handle the incoming items to add to the list
+	var list = req.body.Body.split(',');
+	if(list[0] == 'list' || list[0] == 'List'){
+		sendList();
+	}else{
+		for(var i=0;i<list.length;i++){
+			shoppingList.push(list[i]);
+		}
+		console.log('Items added to the list.');
+	}
+});
+app.listen(process.env.PORT || 3000);
+
