e64ed9b5483e1b6431bbe5bdb677803762888a72
Tom Townsend <tom.towny@gmail.com>
2013-02-16 22:08:09 +0000

    added sample config file

---
+++ config_sample.js
@@ -0,0 +1,7 @@
+module.exports.Credentials = {
+    sid: 'xxx',
+    authToken: 'yyy',
+    incoming: 'number to notify',
+    outgoing: 'your current twilio number',
+    hostname: 'hostname of your webserver'
+};
